7898fc3bc7bfb90c91ea4facfe5e1023e52c2873
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-09-13 01:10:09 -0700

    selected DOM elements with querySelector

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const searchForm = document.getElementById('search-form');
+const searchButton = document.getElementById('search-button');
+const present = document.getElementById('present');
+const future = document.getElementById('future');
