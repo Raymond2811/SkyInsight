5e5187951eda4526aaeb9ac101e8c6c65447aa88
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-09-15 01:31:13 -0700

    added weather data retrieval event listner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,27 @@
-const searchForm = document.getElementById('search-form');
-const searchButton = document.getElementById('search-button');
-const present = document.getElementById('present');
-const future = document.getElementById('future');
+const searchForm = document.querySelector('#search-form');
+const present = document.querySelector('#present');
+const future = document.querySelector('#future');
+const cityInput = document.querySelector('#city-input');
+const apiKey = 'c5a8e528ef21ad697ebae69a55cf1a7e';
+
+searchForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const cityName = cityInput.value;
+    if (cityName){
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data);
+            present.innerHTML =  `
+            <div class='styled-text'>
+                <h2>${data.name} (Date: ${new Date(data.dt * 1000).toLocaleDateString()})<h2>
+                <p>Temperature:${Math.round(data.main.temp - 273.15)}°C </p>
+                <p>Wind speed: ${data.wind.speed} mph</p>
+                <p>Humidity: ${data.main.humidity} </p>
+            </div>
+            `
+        })
+    }
+});
